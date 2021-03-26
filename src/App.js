b9a367479ed064357bf098274d3c8c776f59dd28
abdul ghani <mohammedghani842@gmail.com>
2021-03-26 16:42:40 -0700

    practicing use State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  
+  const  [count, upcount] = useState(0)
+    
+  const incrmt = () => {
+    upcount(count+1);
+  }
+
+
+
   return (
     <div >
-      <h1>hello world
-
-      </h1>
+      <h1>{count}</h1>
+      <button onClick={incrmt}>click me</button>
+      
     </div>
   );
 }
