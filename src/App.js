15c68b98a9f68dec45de114c2b0f45ea44f7e05c
abdul ghani <mohammedghani842@gmail.com>
2021-03-27 08:54:03 -0700

    forms

--- src/App.js
+++ src/App.js
@@ -21,31 +21,43 @@ function App() {
 //  }
 //  setInterval(timeup,1000)
 
-let naam = "click"
-let dsnaam = "nakko karo"
-let purple = "#8e44ad";
-let green = "#34495e"
-const [bg, chbg] = useState(purple)
-const [pnaam,dnaam ] = useState(naam)
-const bgChange = () => {
-  chbg(green)
-  dnaam(dsnaam)
+// let naam = "click"
+// let dsnaam = "nakko karo"
+// let purple = "#8e44ad";
+// let green = "#34495e"
+// const [bg, chbg] = useState(purple)
+// const [pnaam,dnaam ] = useState(naam)
+// const bgChange = () => {
+//   chbg(green)
+//   dnaam(dsnaam)
 
   
+// }
+
+
+// const bgBack = () => {
+//   chbg(purple)
+//   dnaam(naam)
+// }
+const [setNaam,upName ] = useState("")
+const [setName, setFull] = useState()
+const namch = (e) => {
+// console.log(e.target.value)
+upName(e.target.value)
 }
-
-
-const bgBack = () => {
-  chbg(purple)
-  dnaam(naam)
+const stName = () => {
+  setFull(setNaam)
 }
 
 
   return (
-    <div className= "App" style={{ backgroundColor:bg}} >
+    <div className= "App"  >
       {/* <h1 > {stime}</h1> */}
       {/* <button className = "kuch" onClick={timeup} >click me</button>  */}
-      <button onMouseEnter={bgChange} onMouseLeave={bgBack}>{pnaam}</button>
+      {/* <button onMouseEnter={bgChange} onMouseLeave={bgBack}>{pnaam}</button> */}
+      <h1>Hello {setName}</h1>
+      <input onChange={namch} type="text" placeholder="enter your name" />
+      <div><button onClick={stName}>click me</button></div>
       
     </div>
   );
