b3c7514d5203c2a02b8c761e517c6e2e0b6ad404
abdul ghani <mohammedghani842@gmail.com>
2021-03-26 17:15:49 -0700

    useStatees'

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import './App.css';
 
 function App() {
   
-  const  [count, upcount] = useState(0)
+  // const  [count, upcount] = useState(0)
     
-  const incrmt = () => {
-    upcount(count+1);
-  }
-
+  // const incrmt = () => {
+  //   upcount(count+1);
+  // }
+  
+  const time = new Date().toLocaleTimeString(); 
+  
+ const [stime,settime ] = useState(time)
+ const timeup = () => {
+   let newtime = new Date().toLocaleTimeString(); 
+   settime(newtime)
+   
+ }
 
 
   return (
     <div className= "App" >
-      <h1 >{count}</h1>
-      <button className = "kuch" onClick={incrmt}>click me</button>
+      <h1 > {stime}</h1>
+      <button className = "kuch" onClick={timeup} >click me</button>
       
     </div>
   );
